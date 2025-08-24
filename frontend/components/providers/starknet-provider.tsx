86f6f26c10703ed49e7676899909003e5fa5cb05
Stephanie Nwankwo <stephybelle208@gmail.com>
2025-08-24 15:26:32 +0100

    fix: connect wallet

--- frontend/components/providers/starknet-provider.tsx
+++ frontend/components/providers/starknet-provider.tsx
@@ -17,11 +17,12 @@ const chains = [mainnet];
 
 const connectors = [
   new InjectedConnector({
-      options: { id: "argentX", name: "Argent X" },
-    }),
-    new InjectedConnector({
-      options: { id: "braavos", name: "Braavos" },
-    }),
+    options: { id: "argentX", name: "Ready Wallet (formerly Argent)" },
+  }),
+  new InjectedConnector({
+    options: { id: "braavos", name: "Braavos" },
+  })
+
 ]
 
 const StarknetProvider: React.FC<StarknetProviderProps> = ({ children }) => {
@@ -32,22 +33,13 @@ const StarknetProvider: React.FC<StarknetProviderProps> = ({ children }) => {
     new InjectedConnector({
       options: { id: "braavos", name: "Braavos" },
     }),
-    new InjectedConnector({
-      options: { id: "metamask", name: "MetaMask" },
-    }),
-    new InjectedConnector({
-      options: { id: "keplr", name: "Keplr" },
-    }),
-    new InjectedConnector({
-      options: { id: "okxwallet", name: "OKX" },
-    }),
-    new WebWalletConnector({ url: "https://web.argent.xyz" }),
-    ArgentMobileConnector.init({
-      options: {
-        dappName: "Swift",
-        url: "https://swift-whls.vercel.app/",
-      },
-    }),
+    // new WebWalletConnector({ url: "https://web.argent.xyz" }),
+    // ArgentMobileConnector.init({
+    //   options: {
+    //     dappName: "Swift",
+    //     url: "https://swift-whls.vercel.app/",
+    //   },
+    // }),
   ];
 
   return (
